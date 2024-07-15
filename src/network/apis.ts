c6edfce0d384b5a467d8bad84bd65d2947442e1c
GK <balachandra.bt146@gmail.com>
2024-07-15 07:58:58 +0530

    Handling failure cases

--- src/network/apis.ts
+++ src/network/apis.ts
@@ -1,19 +1,23 @@
-import axios from "axios"
-import { END_POINT } from "../constants/endPoints"
-import { sendActionAndDispatch } from "../redux/actions/actions"
-import { ACTIONS, ACTIONS_SUFFIX } from "../redux/constants"
+import axios from 'axios';
+import {END_POINT} from '../constants/endPoints';
+import {sendActionAndDispatch} from '../redux/actions/actions';
+import {ACTIONS, ACTIONS_SUFFIX} from '../redux/constants';
 
 export const getPortfolioHoldings = async () => {
-    sendActionAndDispatch(`${ACTIONS.GET_PORTFOLIO}${ACTIONS_SUFFIX.LOADING}`)
+  sendActionAndDispatch(`${ACTIONS.GET_PORTFOLIO}${ACTIONS_SUFFIX.LOADING}`);
 
-    // can use async await or a promise with .then and catch to send appropriate actions
-    const response = await axios.get(END_POINT.userHoldings)
+  // we can use async await or a promise with .then and catch to send appropriate actions
+  try {
+    const response = await axios.get(END_POINT.userHoldings);
 
     console.log('LOGS:: api response', response.data.data);
-    try {
-        sendActionAndDispatch(`${ACTIONS.GET_PORTFOLIO}${ACTIONS_SUFFIX.SUCCESS}`, response.data.data.userHolding)
-    } catch (error) {
-        sendActionAndDispatch(`${ACTIONS.GET_PORTFOLIO}${ACTIONS_SUFFIX.ERROR}`)
-    }
-
-}
+    if (response.data?.data?.userHolding)
+      sendActionAndDispatch(
+        `${ACTIONS.GET_PORTFOLIO}${ACTIONS_SUFFIX.SUCCESS}`,
+        response.data.data.userHolding,
+      );
+    else throw new Error('Failed or empty data');
+  } catch (error) {
+    sendActionAndDispatch(`${ACTIONS.GET_PORTFOLIO}${ACTIONS_SUFFIX.ERROR}`);
+  }
+};
